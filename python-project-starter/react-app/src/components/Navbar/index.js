bfbc1e06cfabfb7bb3cb1a636c7f57412a918f76
Jesse Brooks <brooks.jesse83@gmail.com>
2022-02-21 11:59:19 -0800

    added search bar styling, usernames first letter into user button icon

--- python-project-starter/react-app/src/components/Navbar/index.js
+++ python-project-starter/react-app/src/components/Navbar/index.js
@@ -1,16 +1,32 @@
 import { NavLink, Link } from 'react-router-dom'
+import { useSelector } from 'react-redux';
 import YouTubeHomeLogo from '../../images/yt_logo.png'
 import '../Navbar/Navbar.css'
+import HamburgerMenuIcon from '../../images/yt-hamburger-menu-icon.png'
 
 function Navbar(){
+    const user = useSelector(state=> state?.session?.user)
+
     return (
 
         <div className='navbar-container'>
+            <button className='navbar-menu-button'>
+                <img src={HamburgerMenuIcon}></img>
+            </button>
             <Link exact to='/'>
                 <img id='image-yt-logo' src={YouTubeHomeLogo}></img>
             </Link>
-            <button className='user-menu-button'>
-                <div className='user-menu-icon'></div>
+            <div className='search-container'>
+                    <input
+                    className='search-bar'
+                    type='text'
+                    placeholder='Search'>
+                    </input>
+            </div>
+            <button className='navbar-menu-button' id='user-menu-button'>
+                <div className='user-menu-icon'>
+                    <p className='user-menu-text'>{user.username[0]}</p>
+                </div>
             </button>
         </div>
     )
